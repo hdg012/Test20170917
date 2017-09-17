c80eceef22575efc3d1a71d4dafadc6fbae294ff
hdg012 <githdg012>
2017-09-17 20:57:20 +0800

    https://github.com/hdg012/Test20170917.git

---
+++ HDG.FrameWork/OracleHelper.cs
@@ -0,0 +1,76 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDG.FrameWork
+{
+    public class OracleHelper
+    {
+        public static string connectionString = ConfigurationManager.AppSettings["ConnBasic"];
+        public static string err = string.Empty;
+
+        public static int ExecuteNonQuery(string sql)
+        {
+            using (OracleConnection conn = new OracleConnection(connectionString))
+            {
+                using (OracleCommand cmd = new OracleCommand(sql, conn))
+                {
+                    try
+                    {
+                        conn.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        return result;
+                    }
+                    catch (OracleException ex)
+                    {
+                        err = ex.Message;
+                        return -1;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+        }
+
+        public static int ExecuteQuery(string sql)
+        {
+            try
+            {
+                OracleConnection conn = new OracleConnection(connectionString);
+                OracleCommand cmd = new OracleCommand(sql, conn);
+                OracleDataReader reader = null;
+                try
+                {
+                    conn.Open();
+                    reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        return int.Parse(reader[0].ToString());
+                    }
+                    return 0;
+                }
+                catch (OracleException ex)
+                {
+                    err = ex.Message;
+                    return -1;
+                }
+                finally
+                {
+                    reader.Close();
+                    conn.Close();
+                }
+            }
+            catch (OracleException ex)
+            {
+                err = ex.Message;
+                return -1;
+            }
+        }
+    }
+}
---
+++ Test20170917/Program.cs
@@ -0,0 +1,60 @@
+﻿using HDG.FrameWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test20170917
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请选择\nA、Insert    B、Update    C、Delete    D、Read\n");
+            string input = string.Empty;
+            string sql = string.Empty;
+            int result = -1;
+            bool read = false;
+            while (true)
+            {
+                read = false;
+                input = Console.ReadLine().ToUpper().Trim();
+                switch (input)
+                {
+                    case "A":
+                        sql = @"INSERT INTO TEST1 VALUES (SYS_GUID(), 'A' || SYSDATE, NULL, TO_CHAR(SYSDATE, 'ss'), SYS_GUID())";
+                        break;
+                    case "B":
+                        sql = @"UPDATE TEST1 T SET T.NAME = 'B' || TO_CHAR(SYSDATE, 'ss') WHERE ROWNUM < 3";
+                        break;
+                    case "C":
+                        sql = @"DELETE TEST1 T WHERE T.NAME LIKE '%B%'";
+                        break;
+                    case "D":
+                        sql = @"SELECT COUNT(1) FROM TEST1";
+                        read = true;
+                        break;
+                    default:
+                        sql = string.Empty;
+                        break;
+                }
+                if (string.IsNullOrEmpty(sql))
+                {
+                    Console.WriteLine("输入无效，请重新输入\nA、Insert    B、Update    C、Delete    D、Read\n");
+                }
+                else
+                {
+                    Console.WriteLine("输入有效，结果如下：");
+                    result = read ? OracleHelper.ExecuteQuery(sql) : OracleHelper.ExecuteNonQuery(sql);
+                    Console.WriteLine("结果为：" + result);
+                    if (result < 0)
+                    {
+                        Console.WriteLine("异常为：" + OracleHelper.err);
+                    }
+                    Console.WriteLine("\n请再次选择\nA、Insert    B、Update    C、Delete    D、Read\n");
+                }
+            }
+        }
+    }
+}
